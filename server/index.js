88ab42b20b910ab7ecb8c044466a5ab85dd0d297
orly07 <delacruzorlando776@gmail.com>
2025-09-03 21:49:35 +0800

    update server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: "http://localhost:5173" })); // This is for development with Vite. Update this to your frontend domain (e.g., "https://your-frontend-domain.com") if you have deployed it.
+app.use(cors({ origin: "https://i-love-you-v2.vercel.app/" })); // This is for development with Vite. Update this to your frontend domain (e.g., "https://your-frontend-domain.com") if you have deployed it.
 app.use(express.json({ limit: "10mb" }));
 
 app.post("/send-email", async (req, res) => {
